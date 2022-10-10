91db54e2ec47b2e6c5f24d6e4bd99f0a983260f7
Misha Merkushin <mikhail.merkushin@sbermarket.ru>
2022-10-10 11:55:21 +0300

    fix Schked spec by adding Redis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,7 +57,9 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
+  redis = Redis.new(url: ENV["REDIS_URL"])
   config.before do
+    redis.flushdb
     Sidekiq::Queues.clear_all
   end
 end
