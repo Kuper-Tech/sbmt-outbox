61b6e55930b21758a2ed08513423f3b1c0b2d080
Misha Merkushin <mikhail.merkushin@sbermarket.ru>
2024-04-17 12:29:17 +0300

    fix: use correct base class for controller

--- app/controllers/sbmt/outbox/root_controller.rb
+++ app/controllers/sbmt/outbox/root_controller.rb
@@ -2,7 +2,7 @@
 
 module Sbmt
   module Outbox
-    class RootController < ApplicationController
+    class RootController < ActionController::Base
       def index
         @local_endpoint = Outbox.config.ui.local_endpoint
         @cdn_url = Outbox.config.ui.cdn_url
