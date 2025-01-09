77e4fa8535cdcc1a7cb2564bcac37b8dcc0b1829
denis.tarasenko <denis.tarasenko@sbermarket.ru>
2025-01-10 01:24:42 +0300

    [DEX-2707] feat: add metrics for delete items

--- spec/jobs/sbmt/outbox/base_delete_stale_items_job_spec.rb
+++ spec/jobs/sbmt/outbox/base_delete_stale_items_job_spec.rb
@@ -25,17 +25,21 @@ describe Sbmt::Outbox::BaseDeleteStaleItemsJob do
     end
   end
 
-  it "deletes items with status 2 and old items with status 1" do
+  it "deletes items with status 2 and old items with status 1 and tracks metrics" do
     expect { job_class.perform_now("OutboxItem") }
       .to change(OutboxItem, :count).by(-2)
+      .and increment_yabeda_counter(Yabeda.outbox.deleted_counter).with_tags(box_name: "outbox_item", box_type: :outbox).by(2)
+      .and measure_yabeda_histogram(Yabeda.outbox.delete_latency).with_tags(box_name: "outbox_item", box_type: :outbox)
   end
 
   context "when an element with status 1 does not retention" do
     let(:created_at) { 6.hours.ago }
 
-    it "doesn't delete item with status 1 but deletes item with status 2" do
+    it "doesn't delete item with status 1 but deletes item with status 2 and tracks metrics" do
       expect { job_class.perform_now("OutboxItem") }
         .to change(OutboxItem, :count).by(-1)
+        .and increment_yabeda_counter(Yabeda.outbox.deleted_counter).with_tags(box_name: "outbox_item", box_type: :outbox).by(1)
+        .and measure_yabeda_histogram(Yabeda.outbox.delete_latency).with_tags(box_name: "outbox_item", box_type: :outbox)
     end
   end
 end
